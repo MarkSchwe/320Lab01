6a06f0c5b26d6b58cb138b5d9f3ba5887e381c9a
MarkSchwe <91101294+MarkSchwe@users.noreply.github.com>
2022-02-08 11:54:48 -0500

    Create deSelsort.h

---
+++ deSelsort.h
@@ -0,0 +1,56 @@
+#include <iostream>
+
+using namespace std;
+
+void valswap(int &a, int &b){
+int swap = a;
+a = b;
+b = swap;
+}
+
+class deSelsort{
+    public: 
+    void selectionSort(int arr[], int size);
+};
+void deSelsort::selectionSort(int arr[], int size){
+int key = 0;
+for(int i = 0; i < size; i++){
+if((i+1 <= size) && (arr[i] > arr[i+1])){
+key = 1;
+}
+}
+int index = 0;
+while(key == 1){
+key = 0;
+for(int i = 0; i <= size; i++){
+if((i+1 <= size) && (arr[i] > arr[i+1])){
+key = 1;
+}
+}
+if(key == 1){
+int largest, smallest = 0;
+int smallestVal = 0;
+int largestVal = 0;
+if(index != size-index){
+int smallestVal = arr[index];
+int largestVal = arr[size-index];
+}
+for(int i = index; i <= size-index;i++){
+if(arr[i] < smallestVal){
+smallest = i;
+smallestVal = arr[i];
+}
+if(arr[i] > largestVal){
+largest = i;
+largestVal = arr[i];
+}
+}
+valswap(arr[index],arr[smallest]);
+valswap(arr[size-index],arr[largest]);
+index++;
+if(index > size-index){
+    index = 0;
+}
+}
+}
+}
